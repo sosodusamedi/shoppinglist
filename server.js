739907c6a9b4d54499e49a50add9db3d13bbf545
Sonny Ghidizzi <sonny.ghidizzi@gmail.com>
2018-06-14 10:58:12 +0200

    scripts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRouter = require('./api');
 const config = require('./config');
 const path = require('path');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 // const parseurl = require('parseurl');
 // const bodyParser = require('body-parser');
 // const expressValidator = require('express-validator');
@@ -12,15 +12,15 @@ const mongoose = require('mongoose');
 const server = express();
 
 // Connect to Database + Change URI
-const url = config.MONGOLAB_URI;
-
-mongoose.connect(url, (err, db) => {
-  if (err) {
-    console.info('Unable to connect to the mongoDB server.', err);
-  } else {
-    console.info('Connected to mongoDB, db:', db);
-  }
-});
+// const url = config.MONGOLAB_URI;
+//
+// mongoose.connect(url, (err, db) => {
+//   if (err) {
+//     console.info('Unable to connect to the mongoDB server.', err);
+//   } else {
+//     console.info('Connected to mongoDB, db:', db);
+//   }
+// });
 
 
 // Set the View Engine
